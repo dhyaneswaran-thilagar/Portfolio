b0e8569c43b2910046bff02fde6248211046e1ed
dhyaneswaran-thilagar <153347436+dhyaneswaran-thilagar@users.noreply.github.com>
2025-01-29 09:16:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['CSE Cyber security Student at SNUC ','Software developer'],
+    strings:['CSE Cyber security Student at SNUC ','Software Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
